abc2f00ea91bb08ee8cb6fcc05c33d563475104c
J3Anonikai <yuya.jonathan.nikai@gmail.com>
2023-04-27 09:03:15 +0900

    red

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'mdn',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(),
     );
